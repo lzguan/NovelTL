f362063e1819cb859cf1d545d31ac0b91dad6fd7
yellowjuice <leo.ziqi@gmail.com>
2025-01-24 02:37:36 -0500

    glossary test in progress

---
+++ scripts/sheisnotawitch/retrieve_gpt.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+import json
+from pprint import pprint
+import sys
+
+client = OpenAI()
+bindir = "bin"
+
+with open(bindir + "/batchresponse_" + sys.argv[1] + ".json", "r") as fin:
+    batchresponse = json.loads(json.load(fin))
+
+batchreturn = client.batches.retrieve(batchresponse['id'])
+
+if batchreturn.status == 'completed':
+    print("Retrieving file " + batchreturn.output_file_id)
+    outputf = client.files.content(batchreturn.output_file_id)
+    with open(bindir + "/glossary_translated_" + sys.argv[1] + ".jsonl", "w") as fout:
+        fout.write(outputf.text)
+    outputf.close()
+else:
+    print("Error: batch not completed yet.")
+    print("Status: " + batchreturn.status)
+    print(batchreturn.request_counts)
